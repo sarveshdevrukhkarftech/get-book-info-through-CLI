653bc0b8d6b4a1f46dce9476de0a0ec72f4851aa
sarveshdevrukhkarftech <sarveshdevrukhkar@fafadiatech.com>
2025-11-17 12:09:22 +0530

    Formate the response of the API.

--- main.py
+++ main.py
@@ -8,19 +8,28 @@ import click
 def search_book(search):
     # Used context managers.
     with requests.Session() as session:
-        base_URL = f"https://openlibrary.org/search.json?q={search}"
-        response = requests.get(base_URL)
+        base_URL = f"https://openlibrary.org/search.json?"
+        params = {"q": {search}}
+        response = requests.get(base_URL, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        docs = data.get("docs")
+        docs = data.get(
+            "docs"
+        )  # This List contains the Dictionaries of Book information.
 
-        for obj in docs:
-            click.echo(f"Book Title: {obj["title"]}\n")
+        click.echo("Here is your 10 search query result.\n\n")  # Heading
+
+        for i in range(10):
+            click.echo(f"\n=== BOOK INFORMATION {i+1} ===")
+            click.echo(f"Book Title: {docs[i]["title"]}")
+            click.echo(f"Author Name: {",".join(docs[i]["author_name"])}")
+            click.echo(f"Published Year: {docs[i]["first_publish_year"]}")
+            click.echo(f"========== | ==========\n")
 
     else:
         click.echo(f"Something Went Wrong: {response.status_code}")
 
 
 if __name__ == "__main__":
-    data = search_book()
+    search_book()
